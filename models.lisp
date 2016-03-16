8aec44faf11f8d134ac6bce73aba97a7cad6c9d5
Jacek Podkanski <ruby.object@googlemail.com>
2016-03-16 18:49:39 +0000

    progress

--- models.lisp
+++ models.lisp
@@ -10,3 +10,14 @@ divided by ONE delimiter character"
 
 (defun last-folder-part (folder)
   (car (last (butlast (split-by-delimiter (directory-namestring folder) *separator*)))))
+
+(defun available-system-names ()
+  (loop for s in (quicklisp:system-list)
+     for z = (slot-value s 'ql-dist:name)
+     collect z))
+
+(defun installed-system-paths ()
+  (remove-if 'null
+             (loop for s in (quicklisp:system-list)
+                for z = (slot-value s 'ql-dist:name)
+                collect (ql:where-is-system z))))
