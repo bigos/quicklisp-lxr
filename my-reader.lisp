3da7057cb25b8a057436a415b38aa9f105c75468
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-19 00:16:00 +0100

    example of read - ing lisp source file

---
+++ my-reader.lisp
@@ -0,0 +1,10 @@
+(defun my-read-file (path)
+  (with-open-file (s path)
+    (loop for zzz = (read s nil :eof)
+       until (eq zzz :eof)
+       collect zzz
+       do
+         (format t "~%~s~%" zzz))))
+
+;; example use
+;; (format t "~%~s~%" (nth 4 (my-read-file "~/Programming/quicklisp-xref/htmlizer.lisp")))
