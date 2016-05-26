445f66504d5e95283497b7f0a355c782932a9c5b
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-27 00:39:49 +0100

    found problem with babel strams sharing same src directory with other package

--- my-reader.lisp
+++ my-reader.lisp
@@ -3,8 +3,8 @@
     (loop for zzz = (read s nil :eof)
        until (eq zzz :eof)
        collect zzz
-       do
-         (format t "~%~s~%" zzz))))
+       ;; do (format t "~%~s~%" zzz)
+         )))
 
 ;; example use
 ;; (format t "~%~s~%" (nth 4 (my-read-file "~/Programming/quicklisp-xref/htmlizer.lisp")))
